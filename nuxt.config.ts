8aee1c4282dfd66b5aa192bf1359a86051d1b001
Kamran Allah Yar <kamranallahyar@outlook.com>
2026-01-05 02:03:27 +0500

    Configure SSR and update baseURL in `nuxt.config.ts`.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
+    ssr:false,
     modules: ["@nuxt/ui", "@nuxt/fonts"],
     colorMode: {
         preference: 'light'
     },
     app: {
+        baseURL: '/barakatnasimkarbala.com/',
         head: {
             link: [
                 {rel: 'icon', type: 'image/png', href: '/logo.png'},

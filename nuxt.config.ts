3f64dd08c157d51246ad5f1a471c2b17bfef9ba9
Kamran Allah Yar <kamranallahyar@outlook.com>
2026-01-07 10:55:31 +0500

    Set up GitHub Actions workflow for deploying Nuxt app to GitHub Pages.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    ssr:false,
+    ssr: false,
     modules: ["@nuxt/ui", "@nuxt/fonts"],
     colorMode: {
         preference: 'light'
     },
     app: {
-        baseURL: '/barakatnasimkarbala.com/',
         head: {
             link: [
                 {rel: 'icon', type: 'image/png', href: '/logo.png'},
